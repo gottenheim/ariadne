b2d7c5d98600dbfcb705393e1bc43b24f1b0f568
gottenheim <luminiscence@gmail.com>
2023-05-30 13:45:12 +0700

    Improved condition tests

--- core/study/scheduled_card_condition_test.go
+++ core/study/scheduled_card_condition_test.go
@@ -9,15 +9,12 @@ import (
 	"github.com/gottenheim/ariadne/libraries/pipeline"
 )
 
-func TestCardScheduledToTodayFilter(t *testing.T) {
+func TestCardScheduledToTodayCondition_ShouldCountCardsScheduledToToday(t *testing.T) {
 	p := pipeline.New()
 
 	cards := card.NewBatchCardGenerator().
 		WithCards(card.NewCardGenerationSpec("Cards scheduled to today", 80, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToToday)).
-		WithCards(card.NewCardGenerationSpec("Cards scheduled and reminded today", 90, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToToday|card.CardExecutedToday)).
-		WithCards(card.NewCardGenerationSpec("Cards scheduled to yesterday and reminded today", 100, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToYesterday|card.CardExecutedToday)).
-		WithCards(card.NewCardGenerationSpec("Cards scheduled to yesterday", 110, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToYesterday)).
-		WithCards(card.NewCardGenerationSpec("Cards scheduled to future", 120, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToTomorrow)).
+		WithCards(card.NewCardGenerationSpec("Cards scheduled to future", 90, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToTomorrow)).
 		Generate()
 
 	briefCards := card.ExtractBriefCards(cards)
@@ -40,11 +37,122 @@ func TestCardScheduledToTodayFilter(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if len(scheduledCardCollector.Items) != 380 {
-		t.Fatal("Filter should find 380 scheduled cards")
+	if len(scheduledCardCollector.Items) != 80 {
+		t.Fatal("Filter should find 80 cards scheduled to today")
 	}
 
-	if len(notScheduledCardCollector.Items) != 120 {
-		t.Fatal("Filter should find 120 not scheduled cards")
+	if len(notScheduledCardCollector.Items) != 90 {
+		t.Fatal("Filter should find 90 cards scheduled to future")
+	}
+}
+
+func TestCardScheduledToTodayCondition_ShouldCountCardsScheduledAndRemindedToday(t *testing.T) {
+	p := pipeline.New()
+
+	cards := card.NewBatchCardGenerator().
+		WithCards(card.NewCardGenerationSpec("Cards scheduled and reminded today", 80, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToToday|card.CardExecutedToday)).
+		WithCards(card.NewCardGenerationSpec("Cards scheduled to future", 90, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToTomorrow)).
+		Generate()
+
+	briefCards := card.ExtractBriefCards(cards)
+
+	timeSource := datetime.NewFakeTimeSource()
+	cardRepo := card.NewFakeCardRepository(cards...)
+
+	scheduledCardCollector := pipeline.NewItemCollector[*card.Card]()
+	notScheduledCardCollector := pipeline.NewItemCollector[card.BriefCard]()
+
+	cardEmitter := pipeline.NewEmitter[card.BriefCard](p, &cardEmitter{cards: briefCards})
+	newCardCondition := pipeline.WithCondition[card.BriefCard](p, cardEmitter, study.ScheduledCardCondition(timeSource, cardRepo))
+
+	pipeline.WithAcceptor[*card.Card](p, pipeline.OnPositiveDecision(newCardCondition), scheduledCardCollector)
+	pipeline.WithAcceptor[card.BriefCard](p, pipeline.OnNegativeDecision(newCardCondition), notScheduledCardCollector)
+
+	err := p.SyncRun()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(scheduledCardCollector.Items) != 80 {
+		t.Fatal("Filter should find 80 cards scheduled and reminded today")
+	}
+
+	if len(notScheduledCardCollector.Items) != 90 {
+		t.Fatal("Filter should find 90 cards scheduled to future")
+	}
+}
+
+func TestCardScheduledToTodayCondition_ShouldCountCardsScheduledToYesterdayAndRemindedToday(t *testing.T) {
+	p := pipeline.New()
+
+	cards := card.NewBatchCardGenerator().
+		WithCards(card.NewCardGenerationSpec("Cards scheduled to yesterday and reminded today", 80, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToYesterday|card.CardExecutedToday)).
+		WithCards(card.NewCardGenerationSpec("Cards scheduled to future", 90, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToTomorrow)).
+		Generate()
+
+	briefCards := card.ExtractBriefCards(cards)
+
+	timeSource := datetime.NewFakeTimeSource()
+	cardRepo := card.NewFakeCardRepository(cards...)
+
+	scheduledCardCollector := pipeline.NewItemCollector[*card.Card]()
+	notScheduledCardCollector := pipeline.NewItemCollector[card.BriefCard]()
+
+	cardEmitter := pipeline.NewEmitter[card.BriefCard](p, &cardEmitter{cards: briefCards})
+	newCardCondition := pipeline.WithCondition[card.BriefCard](p, cardEmitter, study.ScheduledCardCondition(timeSource, cardRepo))
+
+	pipeline.WithAcceptor[*card.Card](p, pipeline.OnPositiveDecision(newCardCondition), scheduledCardCollector)
+	pipeline.WithAcceptor[card.BriefCard](p, pipeline.OnNegativeDecision(newCardCondition), notScheduledCardCollector)
+
+	err := p.SyncRun()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(scheduledCardCollector.Items) != 80 {
+		t.Fatal("Filter should find 80 cards scheduled to yesterday and reminded today")
+	}
+
+	if len(notScheduledCardCollector.Items) != 90 {
+		t.Fatal("Filter should find 90 cards scheduled to future")
+	}
+}
+
+func TestCardScheduledToTodayCondition_ShouldCountCardsScheduledToYesterday(t *testing.T) {
+	p := pipeline.New()
+
+	cards := card.NewBatchCardGenerator().
+		WithCards(card.NewCardGenerationSpec("Cards scheduled to yesterday", 80, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToYesterday)).
+		WithCards(card.NewCardGenerationSpec("Cards scheduled to future", 90, card.LearnCard|card.CardExecutedMonthAgo, card.RemindCard|card.RemindCardScheduledToTomorrow)).
+		Generate()
+
+	briefCards := card.ExtractBriefCards(cards)
+
+	timeSource := datetime.NewFakeTimeSource()
+	cardRepo := card.NewFakeCardRepository(cards...)
+
+	scheduledCardCollector := pipeline.NewItemCollector[*card.Card]()
+	notScheduledCardCollector := pipeline.NewItemCollector[card.BriefCard]()
+
+	cardEmitter := pipeline.NewEmitter[card.BriefCard](p, &cardEmitter{cards: briefCards})
+	newCardCondition := pipeline.WithCondition[card.BriefCard](p, cardEmitter, study.ScheduledCardCondition(timeSource, cardRepo))
+
+	pipeline.WithAcceptor[*card.Card](p, pipeline.OnPositiveDecision(newCardCondition), scheduledCardCollector)
+	pipeline.WithAcceptor[card.BriefCard](p, pipeline.OnNegativeDecision(newCardCondition), notScheduledCardCollector)
+
+	err := p.SyncRun()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(scheduledCardCollector.Items) != 80 {
+		t.Fatal("Filter should find 80 cards scheduled to yesterday")
+	}
+
+	if len(notScheduledCardCollector.Items) != 90 {
+		t.Fatal("Filter should find 90 cards scheduled to future")
 	}
 }
