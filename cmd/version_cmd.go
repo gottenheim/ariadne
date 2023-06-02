fd4ab90bbf0caeadad36e4948803ca2a708a5ba5
gottenheim <luminiscence@gmail.com>
2023-06-02 21:58:06 +0700

    Simple command to display version number

---
+++ cmd/version_cmd.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var showVersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows version number",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("1.0.0")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(showVersionCmd)
+}
